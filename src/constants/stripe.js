d119cba0b43d1043fcbaebd8ac65cceaddcb8fdb
kylebaker616 <kylebaker616@gmail.com>
2021-08-29 14:23:32 -0400

    Fix api key

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 const STRIPE_PUBLISHABLE =
 	process.env.NODE_ENV === 'production'
-	  ? 'pk_live_MY_PUBLISHABLE_KEY'
+	  ? 'pk_test_51JSk3WBTmuyaUYrEPFAdU0cxfR75txhtOjEzVeCStfhkK4lgyNjRmTX8Szag2nGhHh5HBixKhKTILPCUJXKZNdf100jOmTkhco'
 	  : 'pk_test_51JSk3WBTmuyaUYrEPFAdU0cxfR75txhtOjEzVeCStfhkK4lgyNjRmTX8Szag2nGhHh5HBixKhKTILPCUJXKZNdf100jOmTkhco'
 
 export default STRIPE_PUBLISHABLE
